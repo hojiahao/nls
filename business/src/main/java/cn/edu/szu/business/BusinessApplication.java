05f95e8b81277d285576b40aebb77dbc373addc3
hojiahao <hojiahao@outlook.com>
2025-03-05 00:15:37 +0800

    使用HTTP Client来测试接口，IDEA支持创建http文件来测试http接口

--- business/src/main/java/cn/edu/szu/business/BusinessApplication.java
+++ business/src/main/java/cn/edu/szu/business/BusinessApplication.java
@@ -1,13 +1,18 @@
 package cn.edu.szu.business;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@Slf4j
 public class BusinessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BusinessApplication.class, args);
+        ConfigurableEnvironment environment = SpringApplication.run(BusinessApplication.class, args).getEnvironment();
+        log.info("启动成功！");
+        log.info("测试地址：http://localhost:{}{}/hello", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
     }
 
 }
