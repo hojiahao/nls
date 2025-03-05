c60e897dda6a22c5a1484fc9851df7396b3f62cb
hojiahao <hojiahao@outlook.com>
2025-03-05 18:14:41 +0800

    web模块集成Ant Design Vue的图标，安装命令npm install --save @ant-design/icons-vue

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,13 @@ import Antd from 'ant-design-vue'
 import './style.css'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
+import * as Icons from '@ant-design/icons-vue'
 
 const app = createApp(App)
 app.use(Antd).mount('#app')
+
+// 全局使用图标
+const icons = Icons
+for (const icon of icons) {
+    app.component(icon.name, icon)
+}
