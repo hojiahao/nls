07528b8507c5b0c051669c4c034ebce767b277a6
hojiahao <hojiahao@outlook.com>
2025-03-05 19:41:40 +0800

    集成vue-router，增加/home路由，vue-router安装命令 npm install --save vue-router。

--- web/src/main.js
+++ web/src/main.js
@@ -4,9 +4,10 @@ import './style.css'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import * as Icons from '@ant-design/icons-vue'
+import router from './router'
 
 const app = createApp(App)
-app.use(Antd).mount('#app')
+app.use(Antd).use(router).mount('#app')
 
 // 全局使用图标
 const icons = Icons
