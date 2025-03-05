8df23527431ca669ea253e318dc180c4d801ae3b
hojiahao <hojiahao@outlook.com>
2025-03-05 20:14:40 +0800

    vue组件开发，增加the-sider.vue组件

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,9 +7,9 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 111</a-menu-item>
-      <a-menu-item key="2">nav 222</a-menu-item>
-      <a-menu-item key="3">nav 333</a-menu-item>
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
