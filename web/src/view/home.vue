6b6895da98f2021d5583331fbb95019a6fa6bc8f
hojiahao <hojiahao@outlook.com>
2025-03-05 21:01:06 +0800

    演示Vue3的两种用法

--- web/src/view/home.vue
+++ web/src/view/home.vue
@@ -1,6 +1,7 @@
 <template>
   <a-layout>
     <the-header></the-header>
+    /
     <a-layout>
       <the-sider></the-sider>
       <a-layout style="padding: 0 24px 24px">
@@ -12,8 +13,8 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content {{resp}}
-          <a-input v-model:value="resp" @change="onchange" placeholder="Basic usage" />
+          Content {{ resp }}
+          <a-input v-model:value="resp" @change="onChange" placeholder="Basic usage"/>
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -41,10 +42,44 @@ axios.get("http://localhost:8080/nls/query", {
   }
 })
 
-const onchange = () => {
+const onChange = () => {
   console.log(resp.value);
 }
 </script>
+<!--<script>-->
+<!--import {ref, defineComponent} from 'vue';-->
+<!--import axios from "axios";-->
+<!--import {message} from "ant-design-vue";-->
+<!--import TheSider from "../components/the-sider.vue";-->
+<!--import TheHeader from "../components/the-header.vue";-->
+
+<!--export default defineComponent({-->
+<!--  components: {TheSider, TheHeader},-->
+<!--  setup() {-->
+<!--    const resp = ref({});-->
+<!--    axios.get("http://localhost:8080/nls/query", {-->
+<!--      params: {-->
+<!--        mobile: "1"-->
+<!--      }-->
+<!--    }).then((response) => {-->
+<!--      let data = response.data-->
+<!--      if (data.success) {-->
+<!--        resp.value = data.content-->
+<!--      } else {-->
+<!--        message.error(data.msg)-->
+<!--      }-->
+<!--    })-->
+<!--    const onChange = () => {-->
+<!--      console.log(resp.value)-->
+<!--    }-->
+
+<!--    return {-->
+<!--      resp,-->
+<!--      onChange-->
+<!--    }-->
+<!--  }-->
+<!--})-->
+<!--</script>-->
 <style scoped>
 .logo {
   float: left;
