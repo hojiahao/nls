f98d5ef6a551bde68cfe763ba1f94353219c69d4
hojiahao <hojiahao@outlook.com>
2025-03-05 15:33:54 +0800

    使用自定义异常来处理业务异常，重写父类RuntimeExceptionde 方法，不写入堆栈信息，提高性能。

--- business/src/main/java/cn/edu/szu/nls/business/exception/BusinessException.java
+++ business/src/main/java/cn/edu/szu/nls/business/exception/BusinessException.java
@@ -9,4 +9,13 @@ import lombok.EqualsAndHashCode;
 @AllArgsConstructor
 public class BusinessException extends RuntimeException {
     private BusinessExceptionEnum exceptionEnum;
+
+    /**
+     * 不写入堆栈信息，简化异常日志，提高性能
+     * @return
+     */
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
 }
