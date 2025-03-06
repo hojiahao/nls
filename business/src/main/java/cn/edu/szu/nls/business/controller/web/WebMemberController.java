c00bec65a8df7a9a610783ac71c303eaaf2f3d87
hojiahao <hojiahao@outlook.com>
2025-03-06 16:48:45 +0800

    注册接口增加校验短信验证码功能

--- business/src/main/java/cn/edu/szu/nls/business/controller/web/WebMemberController.java
+++ business/src/main/java/cn/edu/szu/nls/business/controller/web/WebMemberController.java
@@ -1,16 +1,20 @@
 package cn.edu.szu.nls.business.controller.web;
 
+import cn.edu.szu.nls.business.enums.SmsCodeUseEnum;
 import cn.edu.szu.nls.business.request.MemberRegisterRequest;
 import cn.edu.szu.nls.business.response.CommonResponse;
 import cn.edu.szu.nls.business.service.MemberService;
+import cn.edu.szu.nls.business.service.SmsCodeService;
 import cn.hutool.crypto.digest.DigestUtil;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/web/member")
 public class WebMemberController {
@@ -18,9 +22,16 @@ public class WebMemberController {
     @Resource
     private MemberService memberService;
 
+    @Resource
+    private SmsCodeService smsCodeService;
+
     @PostMapping("/register")
     public CommonResponse<Object> register(@Valid @RequestBody MemberRegisterRequest request) {
+        log.info("会员注册开始：{}", request.getMobile());
         request.setPassword(DigestUtil.md5Hex(request.getPassword()));
+        // 校验验证码是否有效
+        smsCodeService.validateCode(request.getMobile(), SmsCodeUseEnum.REGISTER.getCode(), request.getCode());
+        log.info("用户{}输入的验证码有效。", request.getMobile());
         memberService.register(request);
         return new CommonResponse<>();
     }
--- business/src/main/java/cn/edu/szu/nls/business/service/SmsCodeService.java
+++ business/src/main/java/cn/edu/szu/nls/business/service/SmsCodeService.java
@@ -9,6 +9,7 @@ import cn.edu.szu.nls.business.exception.BusinessException;
 import cn.edu.szu.nls.business.exception.BusinessExceptionEnum;
 import cn.edu.szu.nls.business.mapper.SmsCodeMapper;
 import cn.edu.szu.nls.business.util.SmsUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.RandomUtil;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -65,5 +67,37 @@ public class SmsCodeService {
         smsCodeMapper.insert(smsCode);
         // 对接短信通道，发送短信
         SmsUtil.sendCode(mobile, code);
+    }
+
+    /**
+     * 验证码校验：
+     * 5分钟内，同一个手机号，同样的用途，未使用过的验证码才算有效
+     * 只校验最后一次验证码
+     */
+    public void validateCode(String mobile, String use, String code) {
+        SmsCodeExample smsCodeExample = new SmsCodeExample();
+        SmsCodeExample.Criteria criteria = smsCodeExample.createCriteria();
+        criteria.andUseEqualTo(use)
+                .andMobileEqualTo(mobile)
+                .andCreatedAtGreaterThan(DateUtil.offsetMinute(new Date(), -5))
+                .andStatusEqualTo(SmsCodeStatusEnum.NOT_USED.getCode());
+        smsCodeExample.setOrderByClause("created_at desc");
+        List<SmsCode> smsCodes = smsCodeMapper.selectByExample(smsCodeExample);
+        if (CollUtil.isNotEmpty(smsCodes)) {
+            SmsCode smsCode = smsCodes.get(0);
+            if (smsCode.getCode().equals(code)) {
+                smsCode.setStatus(SmsCodeStatusEnum.USED.getCode());
+                smsCode.setUpdatedAt(new Date());
+                smsCodeMapper.updateByPrimaryKeySelective(smsCode);
+            }
+            else {
+                log.warn("验证码不正确，手机号:{}， 输入的验证码：{}，用途：{}", mobile, code, use);
+                throw new BusinessException(BusinessExceptionEnum.SMS_CODE_ERROR);
+            }
+        }
+        else {
+            log.warn("验证码未发送或已过期，手机号:{}， 输入的验证码：{}，用途：{}", mobile, code, use);
+            throw new BusinessException(BusinessExceptionEnum.SMS_CODE_EXPIRED);
+        }
     }
 }
